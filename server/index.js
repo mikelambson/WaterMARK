3ea0220898f70f95d4dc9fff88f41341cc6a1aa6
mikelambson <macmicool@gmail.com>
2022-02-09 10:39:35 -0800

    backend dev

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 var mirror = require('browser-terminal-mirror');
 const path = require('path');
+const rp = require('request-promise');
 
 mirror({
     errorPattern: /Warning:/g //discussed below
@@ -10,20 +11,35 @@ mirror({
 app.use( express.static( path.join(__dirname, 'HTML', 'css', 'JS')));
 
 app.get('/', (rec, res)=> {
+    try {
     res.sendFile('public/index.html', {root: __dirname });
-    console.log("...page loaded.");
-    console.info("Console Info")
-});
+    } catch(e) {
+        console.log(500);
+        // Error
+    }
+    });
+
 
 app.get('/style.css', function(req, res) {
-    res.sendFile('public/style.css', {root: __dirname });
-    console.log("...CSS loaded.")    
-});
+    try {
+        res.sendFile('public/style.css', {root: __dirname });
+        } catch(e) {
+            console.log(501);
+            // Error
+        }
+        });
+    
+    
+
 
 app.get('/client.js', function(req, res) {
-    res.sendFile('public/client.js', {root: __dirname });
-    console.log("...JS loaded.")    
-});
+    try {
+        res.sendFile('public/client.js', {root: __dirname });
+        } catch(e) {
+            console.log(502);
+            // Error
+        }
+        });
 
 app.listen(3001, () => {
     console.log("running on port 3001");
