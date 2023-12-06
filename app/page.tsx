63b68515b336e01762b2aed857e858c0e3f41bd9
mikelambson <macmicool@gmail.com>
2023-12-06 13:08:05 -0800

    Update Code

--- app/page.tsx
+++ app/page.tsx
@@ -22,78 +22,87 @@ import {
 
 export default function Home() {
   return (
-    <div className={"pt-24 flex gap-6 px-7 flex-wrap"}>
-      <Ordercount />
-      <div className={"flex flex-grow  gap-6 flex-wrap"}>
-      
-        <Card className={" w-full max-w-5/6"}>
-          <CardHeader>
-            <CardTitle>Flows</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-        
-        <Card className={"w-[350px]"}>
-          <CardHeader>
-            <CardTitle>Create Workorder</CardTitle>
-            <CardDescription>Create mainenance request order.</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form id="maintReq" name="maintenenceRequest">
-              <div className={"grid w-full items-center gap-4"}>
-                <div className={"flex flex-col space-y-1.5"}>
-                  <Label>Request</Label>
-                  <Input
-                    id="reqDetail"
-                    name="request"
-                    type="text"
-                    autoComplete="off"
-                    placeholder="Input request detail"
-                  />
-                </div>
-                <div className={"flex flex-col space-y-1.5"}>
-                  <Label >District</Label>
-                  <Select name="District">
-                    <SelectTrigger id="district">
-                      <SelectValue placeholder="Select" />
-                    </SelectTrigger>
-                    <SelectContent position="popper">
-                      <SelectItem value="west">West</SelectItem>
-                      <SelectItem value="central">Central</SelectItem>
-                      <SelectItem value="east">East</SelectItem>
-                      <SelectItem value="truckee">Truckee</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-              </div>
-            </form>
-          </CardContent>
-          <CardFooter className={"flex justify-between"}>
-            <Button variant="outline" id="maintReqCancel">
-              Cancel
-            </Button>
-            <Button id="maintReqSend">Send</Button>
-          </CardFooter>
-        </Card>
+    <div className={" pt-16"}>
+      <div className={"p-4"}>
+        <h2 className={"text-lg font-semibold text-yellow-800 md:ml-[40vw]"}>Welcome to TCID WaterMARK</h2>
+        <h1 className={"text-2xl font-semibold text-yellow-800 md:ml-[26vw] break-normal"}>
+          Water Measurment Administration & Record Keeping
+        </h1>
+      </div>
+      <div className={"flex gap-6 px-7 flex-wrap"}>      
 
-        <Card>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+        <Ordercount />
+        <div className={"flex flex-grow  gap-6 flex-wrap"}>
+        
+          <Card className={" w-full max-w-5/6"}>
+            <CardHeader>
+              <CardTitle>Flows</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+          
+          <Card className={"w-[350px]"}>
+            <CardHeader>
+              <CardTitle>Create Workorder</CardTitle>
+              <CardDescription>Create mainenance request order.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form id="maintReq" name="maintenenceRequest">
+                <div className={"grid w-full items-center gap-4"}>
+                  <div className={"flex flex-col space-y-1.5"}>
+                    <Label>Request</Label>
+                    <Input
+                      id="reqDetail"
+                      name="request"
+                      type="text"
+                      autoComplete="off"
+                      placeholder="Input request detail"
+                    />
+                  </div>
+                  <div className={"flex flex-col space-y-1.5"}>
+                    <Label >District</Label>
+                    <Select name="District">
+                      <SelectTrigger id="district">
+                        <SelectValue placeholder="Select" />
+                      </SelectTrigger>
+                      <SelectContent position="popper">
+                        <SelectItem value="west">West</SelectItem>
+                        <SelectItem value="central">Central</SelectItem>
+                        <SelectItem value="east">East</SelectItem>
+                        <SelectItem value="truckee">Truckee</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+            <CardFooter className={"flex justify-between"}>
+              <Button variant="outline" id="maintReqCancel">
+                Cancel
+              </Button>
+              <Button id="maintReqSend">Send</Button>
+            </CardFooter>
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
       </div>
     </div>
   );
