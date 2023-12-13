eab0c8298951cc56c3dbc2d38d7d2371cd70e68d
mikelambson <macmicool@gmail.com>
2023-12-13 08:10:33 -0800

    Update Code

--- app/scheduling/_components/schedule-orders/OrderCard.tsx
+++ app/scheduling/_components/schedule-orders/OrderCard.tsx
@@ -38,7 +38,7 @@ const OrderCard = ({
 
     return ( 
         <div
-            className={ cn(" rounded-md drop-shadow-md", isDarkMode ? "bg-gray-300" :"bg-gray-800")}
+            className={ cn(" rounded-md drop-shadow-md", isDarkMode ? "bg-stone-300" :"bg-gray-800")}
             {...draggableProps} {...dragHandleProps} ref={innerRef}
         >
             <div className="grid grid-flow-row grid-rows-4 grid-cols-[2rem,1fr,1fr,1fr] 
@@ -46,10 +46,10 @@ const OrderCard = ({
                 <div className={cn("col-start-1 row-start-1 row-span-4 pt-6", isDarkMode ? "text-gray-400" : "text-gray-700")}>
                     <DragIcon />
                 </div>
-                <div className="col-span-3 text-bottom row-start-1 col-start-2 font-semibold">{order.laterals}</div>
-                <div className={cn("col-span-3 text-bottom row-start-2 border-b-2 font-semibold", isDarkMode ? "text-lime-900/95 border-gray-200" : " text-lime-400/50 border-gray-600")}>{order.remarks}</div>
+                <div className="col-span-3 text-bottom row-start-1 col-start-2 text-foreground/80 font-semibold">{order.laterals}</div>
+                <div className={cn("col-span-3 text-bottom row-start-2 border-b-2 font-semibold", isDarkMode ? "text-lime-900/95 border-gray-200" : " text-lime-400/40 border-gray-600")}>{order.remarks}</div>
                 <div className="col-start-1 row-start-4"></div>
-                <div className={cn("row-span-2 col-start-2 row-start-3 border-r-2 text-sm pt-[.3rem]", isDarkMode ? "border-gray-200" : "border-gray-600" )}>{order.orderTimestamp}</div>
+                <div className={cn("row-span-2 col-start-2 row-start-3 border-r-2 text-sm pt-2", isDarkMode ? "border-gray-200" : "border-gray-600" )}>{order.orderTimestamp}</div>
                 <div className={cn("col-start-3 row-start-3 border-r-2 pl-1 font-medium", isDarkMode ? "border-gray-200" : "border-gray-600" )}>Order# {id}</div>
                 <div className={cn("col-span-4 row-start-3 pl-1 font-medium", isDarkMode ? "border-gray-200" : "border-gray-600" )}>{order.approxCfs} CFS</div>
                 <div className={cn("col-start-3 row-start-4 border-r-2 pl-1", isDarkMode ? "border-gray-200" : "border-gray-600" )}>{order.district} | {order.status}</div>
@@ -61,7 +61,7 @@ const OrderCard = ({
                         </div>
                 </div>
                 <div className={`col-start-1 col-span-4 row-start-5 relative overflow-hidden transition-all ${isDetailsVisible ? cn("h-auto opacity-100 border-t-2 rounded-b-md drop-shadow-md", isDarkMode ? "border-gray-200" : "border-gray-600") : 'h-0 opacity-0'} duration-300 ease-in-out`}>
-                    <div className={cn("p-1 ", isDarkMode ? "bg-stone-400 font-medium" :"bg-zinc-800")}>
+                    <div className={cn("p-1 ", isDarkMode ? "bg-gray-300" :"bg-zinc-800")}>
                     Irrigator: {order.details.irrigatorsName}<br />
                     Owner: {order.details.ownersName}<br />
                     Ordered AF: {order.details.approxAf}<br />
