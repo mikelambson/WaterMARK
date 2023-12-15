35e39f1f29fd0f690307dfa249cfb9c94842f3a0
mikelambson <macmicool@gmail.com>
2023-12-15 09:22:29 -0800

    Update Code

--- app/scheduling/_components/UpdateFlows.tsx
+++ app/scheduling/_components/UpdateFlows.tsx
@@ -12,23 +12,20 @@ import {
     TableRow, } from "@/components/ui/table";
 import useFlowsStore from '@/store/opsFlowsStore';
 import { formatNumber } from "@/lib/basicFunctions"; 
+import { ToastAction } from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
 
 const UpdateFlowsWM = () => {
-    // Access the flows array from the store
+    // Access the flows array & updateFlow function from the store
     const flows = useFlowsStore((state) => state.flows);
-    // Access the updateFlow function from the store
-//   const updateFlow = useFlowsStore((state) => state.updateFlow);
-
-//   const handleUpdate = (flowId: string, newValue: number) => {
-//     // Call the updateFlow function with the relevant parameters
-//     updateFlow(flowId, newValue);
-//   };
-
+    const updateFlow = useFlowsStore((state) => state.updateFlow);
+    // Toast functions
+    const { toast } = useToast();
 
     return ( 
         <div>
             <Table>
-                <TableCaption><Button variant={"secondary"}>Update All</Button></TableCaption>
+                <TableCaption><Button variant={"destructive"} className="active:bg-destructive/30">Update Database</Button></TableCaption>
                 <TableHeader>
                     <TableRow>
                     <TableHead>Name</TableHead>
@@ -44,18 +41,57 @@ const UpdateFlowsWM = () => {
                 <TableBody>
                     {flows.map((row) => (
                         <TableRow key={row.id}>
-                        <TableCell className="font-medium">{row.name}</TableCell>
-                        <TableCell>{row.remoteSource}</TableCell>
-                        <TableCell>{formatNumber(row.remoteValue)}&ensp;{formatNumber(row.remoteValue) !== "" ? row.type : "--"}</TableCell>
-                        <TableCell>
-                            <Switch 
-                                checked={row.override}
-                            />
-                        </TableCell>
-                        <TableCell>{row.manualTimestamp !== null ? row.manualTimestamp : "--"}</TableCell>
-                        <TableCell className="text-right">{formatNumber(row.manualValue)}&ensp;{formatNumber(row.manualValue) !== "" ? row.type : "--"}</TableCell>
-                        <TableCell><Input /></TableCell>
-                        <TableCell><Button>Update</Button></TableCell>
+                            <TableCell className="font-medium">{row.name}</TableCell>
+                            <TableCell>{row.remoteSource}</TableCell>
+                            <TableCell>{formatNumber(row.remoteValue)}&ensp;{formatNumber(row.remoteValue) !== "" ? row.type : "--"}</TableCell>
+                            <TableCell>
+                                <Switch 
+                                    checked={row.override}
+                                    onCheckedChange={() => {
+                                        updateFlow(row.id, { override: !row.override });                                     
+                                        toast({
+                                            title: row.name,
+                                            description: `Set Override: ${!row.override}`,
+                                        });
+                                    }}
+                                    disabled={row.remoteSource === "Manual"}
+                                />
+                            </TableCell>
+                            <TableCell>{row.manualTimestamp !== null ? row.manualTimestamp : "--"}</TableCell>
+                            <TableCell className="text-right">{formatNumber(row.manualValue)}&ensp;{formatNumber(row.manualValue) !== "" ? row.type : "--"}</TableCell>
+                            <TableCell>
+                                <Input id={row.id + "manualinput"} placeholder="New Value" 
+                                className="border-foreground/50" />
+                            </TableCell>
+                            <TableCell>
+                                <Button 
+                                     className="active:bg-stone-500"
+                                     onClick={() => {
+                                        const inputValue = document.getElementById(row.id + "manualinput") as HTMLInputElement | null;
+                            
+                                        // Check if inputValue is a number and not null
+                                        if (inputValue && !isNaN(Number(inputValue.value))) {
+                                            const newValue = Number(inputValue.value);
+                                            updateFlow(row.id, { manualValue: newValue });
+
+                                            toast({
+                                                title: "Manual Update",
+                                                description: `${row.name}`,
+                                            })
+                                        } else {
+                                            // Handle the case when the input is not a valid number
+                                            toast({
+                                                variant: "destructive",
+                                                title: "Uh oh! That's not a valid number.",
+                                                description: "Please enter a valid number into the input box.",
+                                                action: <ToastAction altText="Try again">Try again</ToastAction>,
+                                            })
+                                        }
+                                    }}
+                                >
+                                    Save
+                                </Button>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
