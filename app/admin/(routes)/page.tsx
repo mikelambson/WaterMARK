da3c34eb27aabdd7af0cdfc18171d04d9d673d1c
BuildTools <unconfigured@null.spigotmc.org>
2024-01-08 04:09:19 -0800

    Update Code

--- app/admin/(routes)/page.tsx
+++ app/admin/(routes)/page.tsx
@@ -1,8 +1,14 @@
 export default function Admin() {
   return (
     <div className="p-1">
-      <h1 className={"text-2xl font-semibold text-yellow-800 md:text-center "}>Administrative Staff Dashboard</h1>
-      <p className="text-justify">This is an administrative (office staff) Dashboard which will be designed with quick access to  info for some of the most asked questions about orders or other operations functions.</p>
+      <h1 className={"text-2xl font-semibold text-yellow-800 md:text-center "}>
+        Administrative Staff Dashboard
+      </h1>
+      <p className="text-center">
+        This is an administrative (office staff) Dashboard which will be
+        designed with quick access to info for some of the most asked questions
+        about orders or other operations functions.
+      </p>
     </div>
-    );
-};
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,27 @@ import Ordercount from "@/components/cards/Ordercount";
 import LakeForcast from "@/components/cards/LakeForcast";
 import Demand from "./scheduling/_components/Demand";
 
-
 export default function Home() {
   return (
     <div className={" pt-16"}>
       <div className={"p-4 text-center"}>
-        <h2 className={" font-serif text-lg font-semibold text-yellow-800"}>Welcome to TCID WaterMARK</h2>
-        <h1 className={" font-serif text-sm md:text-2xl font-semibold text-yellow-800 break-normal"}>
+        <h2 className={" font-serif text-lg font-semibold text-yellow-800"}>
+          Welcome to TCID WaterMARK
+        </h2>
+        <h1
+          className={
+            " font-serif text-sm md:text-2xl font-semibold text-yellow-800 break-normal"
+          }
+        >
           Water Measurment Administration & Record Keeping
         </h1>
       </div>
       <div className={"flex flex-row justify-center gap-4 flex-wrap px-[1%]"}>
-        <div className={"flex flex-col gap-4 min-w-fit w-full md:w-3/4 md:max-w-screen-lg"}>       
+        <div
+          className={
+            "flex flex-col gap-4 min-w-fit w-full md:w-3/4 md:max-w-screen-lg"
+          }
+        >
           <SysInfo className={" w-full"} />
           <LakeForcast className={"w-full"} />
         </div>
@@ -23,7 +32,7 @@ export default function Home() {
           <Demand />
           <Ordercount className="w=full" />
         </div>
-      </div>  
+      </div>
     </div>
   );
 }
