5b654e66d56aa33e9e537f0a45fe71138cf5a95d
mikelambson <macmicool@gmail.com>
2024-03-08 13:36:07 -0800

    Update Code

--- app/deliveries/_components/ScheduledDeliveries.tsx
+++ app/deliveries/_components/ScheduledDeliveries.tsx
@@ -53,8 +53,8 @@ const Deliveries = ({ id, columns, index }: Properties) => {
 
 
     return (   
-        <div className="relative w-full min-h-[71svh] md:min-h-[84.5svh] p-2 bg-foreground/10 dark:bg-foreground/75 rounded-md shadow-md">
-        <Tabs key={("scheduledcolumn" + selectedSheet.name)} defaultValue={"1"} className="w-full h-full pb-4">
+        <div className="relative w-full min-h-[71svh] md:min-h-[84svh] p-2 bg-foreground/10 dark:bg-foreground/75 rounded-md shadow-md">
+        <Tabs key={("scheduledcolumn" + selectedSheet.name)} defaultValue={"1"} className="w-full h-full pb-0">
                 <TabsList className={"mx-auto inline-flex w-full px-2 bg-stone-400 dark:bg-zinc-800 cursor-default"}>
                     <div key={"left"} className="inline-flex">
                         <h2 key={"label"} className={" font-semibold text-slate-600 dark:text-gray-400 self-center mr-2"}>
@@ -89,7 +89,7 @@ const Deliveries = ({ id, columns, index }: Properties) => {
                 </TabsList>
 
                 <TabsContent  value={selectedSheet.id === 0 ? "1" : "0"} className="h-full">
-                    <div className="w-full min-h-[62svh] md:min-h-[73svh] h-full flex flex-col justify-center text-center text-6xl md:text-8xl rounded-md bg-black/25 shadow-md">
+                    <div className="w-full min-h-[62svh] md:min-h-[75svh] h-full flex flex-col justify-center text-center text-6xl md:text-8xl rounded-md bg-black/25 shadow-md">
                     {optionSelection(selectedDistrict)} 
                     </div>
                 </TabsContent>
