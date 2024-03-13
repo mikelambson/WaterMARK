1dc3d0bff168e2243bef0de908a09d4b5bb066d4
mikelambson <macmicool@gmail.com>
2024-03-13 15:59:00 -0700

    Update Code

--- app/deliveries/_components/UpdateMeasurements.tsx
+++ app/deliveries/_components/UpdateMeasurements.tsx
@@ -17,7 +17,7 @@ const UpdateMeasurements: React.FC = () => {
 
 
     return (
-        <DialogContent className="max-w-[95%] h-[95%]">
+        <DialogContent className="max-w-[65%] h-[85%] dark:border-gray-600">
             <Tabs defaultValue="est" className="w-full h-full">
                 <DialogHeader>
                     <DialogTitle className="text-center text-card-alternative text-xl">
@@ -47,7 +47,8 @@ const UpdateMeasurements: React.FC = () => {
                 </TabsContent>
                 <TabsContent value="poly">
                     <p className="text-center text-lg">Polystick</p>
-                    <div className="grid grid-cols-2 gap-2 max-w-96 mx-auto pt-3">
+                    
+                    <div className="grid grid-cols-2 gap-2 w-80 mx-auto pt-3">
                     <Label htmlFor="polyread">Polystick Reading:</Label>
                     <Label htmlFor="polywidth">Width:</Label>
                     <Input id="polyread" defaultValue={"0.00"} />
@@ -70,8 +71,32 @@ const UpdateMeasurements: React.FC = () => {
                 </TabsContent>
                 <TabsContent value="overTheBoards">
                     <p className="text-center text-lg">Over The Boards</p>
-                    <Label htmlFor="otbCFS">OTB CFS:</Label>
-                    <Input id="otbCFS" defaultValue={"0.00"} />
+                    <div className="grid grid-cols-2 gap-2 w-72 mx-auto pt-3 items-center">
+                        <Label htmlFor="otbU">Upstream:</Label>
+                        <Input id="otbU" defaultValue={"2.00"} />
+                        <Label htmlFor="otbB">Boards:</Label>
+                        <Input id="otbB" defaultValue={"3.00"} />
+                        <Label htmlFor="otbL">Length:</Label>
+                        <Input id="otbL" defaultValue={"4.00"} />
+                    </div>
+                    <div className="mt-2 text-center text-lg">
+                        <Button className="mt-2" onClick={() => {
+                            const otbU = document.getElementById("otbU") as HTMLInputElement;
+                            const otbB = document.getElementById("otbB") as HTMLInputElement;
+                            const otbL = document.getElementById("otbL") as HTMLInputElement;
+                            const otbcalc = document.getElementById("otbcalc") as HTMLParagraphElement;
+                            const result = Math.round((3.33 * parseFloat(otbL.value) * Math.pow((parseFloat(otbB.value) - parseFloat(otbU.value)), 1.5)) * 100 ) / 100;
+                            otbcalc.innerHTML = `The flow over the boards is: ${result} CFS.`;
+                        }}>Submit</Button>
+                        <p id="otbcalc" className="text-lg" />
+                    </div>
+                    <div>
+                    Q=3.33LH^1.5 <br />
+                    Q=3.33*L*(B-U)^1.5 <br />
+                    U= Upstream <br />
+                    B= Boards<br />
+                    L= Length<br />
+                    </div>
                 </TabsContent>
                 <TabsContent value="submerged">
                     <p className="text-center text-lg">Submerged</p>
