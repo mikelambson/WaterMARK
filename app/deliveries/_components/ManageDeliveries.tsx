695936992b16e941770ed0d29e06513b79e6f77f
mikelambson <macmicool@gmail.com>
2024-03-14 12:49:28 -0700

    Update Code

---
+++ app/deliveries/_components/ManageDeliveries.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface Props {
+    // Define your component props here
+}
+
+const ManageDeliveries: React.FC<Props> = () => {
+    // Component logic goes here
+
+    return (
+        <div>
+            {/* JSX content goes here */}
+        </div>
+    );
+};
+
+export default ManageDeliveries;
