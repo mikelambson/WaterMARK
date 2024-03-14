af9e3ef1a2c6276e7e2128a12e5e26e3df10ef59
mikelambson <macmicool@gmail.com>
2024-03-14 08:03:03 -0700

    Update Code

--- app/deliveries/_components/UpdateMeasurements.tsx
+++ app/deliveries/_components/UpdateMeasurements.tsx
@@ -52,8 +52,34 @@ const UpdateMeasurements: React.FC = () => {
                 </DialogHeader>
                 <TabsContent value="est">
                     <p className="text-center text-lg">Estimation</p>
-                    <Label htmlFor="estCFS">Estimated CFS:</Label>
-                    <Input id="estCFS" defaultValue={"0.00"} />
+                    <div className="grid grid-cols-2 gap-2 w-72 mx-auto pt-3 items-center">
+                        <Label htmlFor="estCFS">Estimated CFS:</Label>
+                        <Input id="estCFS" defaultValue={"0.00"} />
+                    </div>
+                    <p className="mt-2 border-t-2 pt-2 text-center text-lg">Timed Flow</p>
+                    <div className="grid grid-cols-2 gap-2 w-72 mx-auto pt-3 items-center">
+                        <Label htmlFor="estD">Distance ft:</Label>
+                        <Input id="estD" defaultValue={"0.00"} />
+                        <Label htmlFor="estS">Time in seconds:</Label>
+                        <Input id="estS" defaultValue={"0.00"} />
+                        <Label htmlFor="estH">Water Height</Label>
+                        <Input id="estH" defaultValue={"0.00"} />
+                        <Label htmlFor="estW">Average Width</Label>
+                        <Input id="estW" defaultValue={"0.00"} />
+                    </div>
+                    <div className="mt-2 text-center text-lg">
+                        <Button className="mt-2" onClick={() => {
+                            const estCFS = document.getElementById("estCFS") as HTMLInputElement;
+                            const estD = document.getElementById("estD") as HTMLInputElement;
+                            const estS = document.getElementById("estS") as HTMLInputElement;
+                            const estH = document.getElementById("estH") as HTMLInputElement;
+                            const estW = document.getElementById("estW") as HTMLInputElement;
+                            const estcalc = document.getElementById("estcalc") as HTMLParagraphElement;
+                            const result = Math.round((parseFloat(estD.value) * parseFloat(estH.value) * parseFloat(estW.value) / parseFloat(estS.value)) * 100 ) / 100;
+                            estcalc.innerHTML = `The flow is: ${result} CFS.`;
+                        }}>Submit</Button>
+                        <p id="estcalc" className="text-lg" />
+                    </div>
                     <div className=" border-t-2 mt-4 text-sm">
                     Instructions: <br />
                     </div>
@@ -62,10 +88,10 @@ const UpdateMeasurements: React.FC = () => {
                     <p className="text-center text-lg">Polystick</p>
                     
                     <div className="grid grid-cols-2 gap-2 w-72 mx-auto pt-3 items-center">
-                    <Label htmlFor="polyread">Polystick Reading:</Label>
-                    <Input id="polyread" defaultValue={"0.00"} />
-                    <Label htmlFor="polywidth">Width:</Label>
-                    <Input id="polywidth" defaultValue={"0.00"} />
+                        <Label htmlFor="polyread">Polystick Reading:</Label>
+                        <Input id="polyread" defaultValue={"0.00"} />
+                        <Label htmlFor="polywidth">Width:</Label>
+                        <Input id="polywidth" defaultValue={"0.00"} />
                     </div>
 
                     <div className="mt-2 text-center text-lg">
