e211e6367796ccc1ffeefe5328b38d48a68c4d7e
mikelambson <macmicool@gmail.com>
2024-03-20 16:28:09 -0700

    Update Code

--- app/account/(routes)/page.tsx
+++ app/account/(routes)/page.tsx
@@ -1,12 +1,15 @@
-//  ./app/login/page.tsx
 "use client";
 import { useRole } from "@/components/nav/RoleContext"; // Import useRole
+import { Button } from "@/components/ui/button";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+
+
 
 export default function Login() {
   const { userRole, setUserRole } = useRole(); // Access userRole and setUserRole
 
-  const handleRoleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setUserRole(event.target.value); // Set the user's role
+  const handleRoleChange = (role:string) => {
+    setUserRole(role); // Set the user's role
   };
 
   // Define an array of role options
@@ -23,31 +26,37 @@ export default function Login() {
     // Add other roles as needed
   ];
 
-  return (
-    <div className="">
-      
-      <div className={"pt-10"}>
+    return (
         <div>
-          <h1 className={"text-2xl font-semibold text-yellow-800 md:text-center "}>Profile/Login Page</h1> 
-        </div>
-      
-        <div className="mt-10 flex gap-2 text-center justify-center">
-          <label htmlFor="roleSelect">Select Role:</label>
-          <select
-            id="roleSelect"
-            name="role"
-            value={userRole}
-            onChange={handleRoleChange}
-          >
-            {roleOptions.map((role) => (
-              <option key={role} value={role}>
-                {role}
-              </option>
-            ))}
-          </select>
-          <button>Login</button>
-        </div>
-      </div>
-    </div>
-  );
+            <div className={"pt-10"}>
+                <h1 className={"text-2xl font-semibold text-yellow-800 md:text-center "}>
+                    Profile/Login Page
+                </h1> 
+            </div>
+
+            <Button disabled>
+                    Login
+                </Button>
+        
+            <div className="mt-10 flex gap-2 text-center justify-center items-center">
+                <label>Select Role:</label>
+                <Select onValueChange={handleRoleChange}>
+                    <SelectTrigger className="w-48">
+                        <SelectValue id="selectedRole" placeholder={userRole} />
+                    </SelectTrigger>
+                    <SelectContent>
+                        {roleOptions.map((role) => (
+                            <SelectItem
+                                key={role}
+                                value={role}                                
+                            >
+                                {role}
+                            </SelectItem>
+                        ))}
+                    </SelectContent>
+                </Select>
+                
+            </div>
+        </div>  
+    );
 }
