8dd53d5432f9074ff6a06c4da139b31597a6bab1
mikelambson <macmicool@gmail.com>
2024-03-22 09:39:36 -0700

    Update Code

--- app/scheduling/(routes)/page.tsx
+++ app/scheduling/(routes)/page.tsx
@@ -124,7 +124,7 @@ const Scheduling = () => {
                                 <span className="sr-only">Increase</span>
                             </Button>
                             </div>
-                            <div className="mt-3 h-[120px]">
+                            <div className="mt-3 h-[50svh]">
                             {/* <ResponsiveContainer width="100%" height="100%">
                                 <BarChart data={data}>
                                 <Bar
