7b8bab7d00e46976a2bdee90589c8aa3d7f82d11
mikelambson <macmicool@gmail.com>
2024-04-19 16:17:11 -0700

    Update Code

--- components/nav/Sidebar.tsx
+++ components/nav/Sidebar.tsx
@@ -22,15 +22,10 @@ export const Sidebar: React.FC<NavigationSidebarProps> = ({ sideLinks }) => {
   const pathname = usePathname();
 
   const sidebarBgColor = isDarkMode ? "bg-stone-400/60" : "bg-zinc-800";
-  const sidebarBorder = isDarkMode
-    ? "border-neutral-400"
-    : "border-neutral-700";
-  const sidebarHover = isDarkMode
-    ? "hover:text-yellow-400"
-    : "hover:text-yellow-300";
+  
 
-  const sidebarStyle = `${sidebarBgColor} px-[1px] text-center border-solid border-r-[.5px] ${sidebarBorder} flex flex-col`;
-  const sidebarItem = `${sidebarHover} duration-200 cursor-pointer flex flex-col items-center drop-shadow-md`;
+  const sidebarStyle = `bg-stone-400/60 dark:bg-zinc-800 px-[1px] text-center border-solid border-r-[.5px] border-neutral-400 dark:border-neutral-700 flex flex-col`;
+  const sidebarItem = `hover:text-yellow-800 dark:hover:text-yellow-300 duration-200 cursor-pointer flex flex-col items-center drop-shadow-md`;
 
   // const permission = await deliveriesPermission();
 
@@ -47,12 +42,12 @@ export const Sidebar: React.FC<NavigationSidebarProps> = ({ sideLinks }) => {
               <div
                 key={item.id}
                 className={cn(
-                  "py-2 border-r border-b border-t rounded-r-sm ",
+                  "py-2 border-r border-b border-t rounded-r-sm -ml-1 -mr-[.75px] pl-1",
                   pathname === item.link
                     ? cn(
                         isDarkMode
-                          ? "border-t-yellow-50/5 border-b-yellow-900/10 border-r-yellow-700/40 bg-gradient-to-l from-yellow-700/30 via-yellow-700/20 to-yellow-700/10"
-                          : "border-t-orange-50/5 border-b-orange-950/20 border-r-orange-300/40 bg-gradient-to-l from-orange-300/30 via-orange-300/20 to-orange-300/10"
+                          ? "border-b-yellow-950/60 border-t-orange-100/60 border-r-orange-100/70 bg-gradient-to-l from-yellow-950/60 via-yellow-950/50 to-yellow-950/40"
+                          : "border-t-orange-50/5 border-b-orange-950/20 border-r-orange-300/40 bg-gradient-to-l from-orange-300/40 via-orange-300/30 to-orange-300/20"
                       )
                     : "border-none hover:scale-90"
                 )}
@@ -64,7 +59,7 @@ export const Sidebar: React.FC<NavigationSidebarProps> = ({ sideLinks }) => {
                     `${sidebarItem}`,
                     pathname === item.link
                       ? cn(
-                          isDarkMode ? "text-yellow-900" : "text-orange-300/95"
+                          isDarkMode ? "text-orange-300" : "text-orange-300/95"
                         )
                       : cn(isDarkMode ? "text-zinc-500" : "text-stone-500")
                   )}
