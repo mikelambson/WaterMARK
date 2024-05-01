71b9875b06964bd67b981c0fca948b109b642a91
mikelambson <macmicool@gmail.com>
2024-05-01 07:39:55 -0700

    Update Code

--- components/nav/AvatarMenu.tsx
+++ components/nav/AvatarMenu.tsx
@@ -8,10 +8,14 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { FaBars, FaTimes, FaBell, FaUserCircle } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { BsInfoLg } from "react-icons/bs";
+import { BiMessageDetail } from "react-icons/bi";
+import { RiListSettingsLine, RiUserSettingsLine } from "react-icons/ri";
 import { LogOut, User } from "lucide-react";
-import { useRole, useRoleStore } from "@/components/nav/RoleContext";
+import { useRoleStore } from "@/components/nav/RoleContext";
 import Link from "next/link";
+import { MdHistory } from "react-icons/md";
 
 
 const AvatarMenu = () => {
@@ -36,13 +40,24 @@ const AvatarMenu = () => {
                         </div>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent className="w-56">
-                        <DropdownMenuLabel>Account</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
+                        <DropdownMenuLabel className={"ml-2 text-lg font-semibold text-yellow-800"}>
+                            WaterMARK
+                        </DropdownMenuLabel>
+                        <Link href="/about">
+                            <DropdownMenuItem>
+                                <BsInfoLg className="mr-2 h-4 w-4" />
+                                About
+                            </DropdownMenuItem>
+                        </Link>
+                        <DropdownMenuSeparator />                       
                             {userRole !== "Anonymous" && (
                             <>
+                                <DropdownMenuLabel className={"text-center"}>
+                                    Account
+                                </DropdownMenuLabel>
                                 <Link href="/account">
                                     <DropdownMenuItem>
-                                        <User className="mr-2 h-4 w-4" />
+                                        <User className="mx-2 h-4 w-4" />
                                         <span>
                                             Profile
                                         </span>
@@ -53,35 +68,41 @@ const AvatarMenu = () => {
                                 </Link>
                                 <Link href="/account/notifications">
                                     <DropdownMenuItem>
+                                        <MdHistory className="mx-2 h-4 w-4" />
                                         Notification History
                                     </DropdownMenuItem>
                                 </Link>
                                 <Link href="/account/messages">
                                     <DropdownMenuItem>
+                                    <BiMessageDetail className="mx-2 h-4 w-4" />
                                         Messages
                                     </DropdownMenuItem>
                                 </Link>
                                 <Link href="/account/settings">
                                     <DropdownMenuItem>
+                                        <RiListSettingsLine className="mx-2 h-4 w-4" />
                                         User Settings
                                     </DropdownMenuItem>
                                 </Link>
+                                <DropdownMenuLabel className={"text-center"}>
+                                    Welcome {userRole}
+                                </DropdownMenuLabel>
                                 <DropdownMenuSeparator />
                             </>
-                            )}
+                            )}   
                         <Link href="/">
-                        <DropdownMenuItem 
-                            onClick={() => {
-                                userRole !== "Anonymous" 
-                                ? setUserRole("Anonymous") 
-                                : setUserRole("Watermaster")
-                        }}>
-                            <LogOut className="mr-2 h-4 w-4" />
-                            <span>
-                                {userRole !== "Anonymous" ? "Logout" : "Login"}
-                            </span>
-                            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-                        </DropdownMenuItem>
+                            <DropdownMenuItem 
+                                onClick={() => {
+                                    userRole !== "Anonymous" 
+                                    ? setUserRole("Anonymous") 
+                                    : setUserRole("Watermaster")
+                            }}>
+                                <LogOut className="mr-2 h-4 w-4" />
+                                <span>
+                                    {userRole !== "Anonymous" ? "Logout" : "Login"}
+                                </span>
+                                <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                            </DropdownMenuItem>
                         </Link>
                     </DropdownMenuContent>
                 </DropdownMenu> );
