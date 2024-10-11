679c2d478768211649335f8d36014fce9279a869
mikelambson <macmicool@gmail.com>
2024-10-11 09:51:06 -0700

    Update Code

--- app/testing/(routes)/login/page.tsx
+++ app/testing/(routes)/login/page.tsx
@@ -1,68 +1,16 @@
-import { Button } from "@/components/ui/button";
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-
 import LoginForm from "@/app/testing/_components/login/loginForm"
-
-
 export default function Login() {
 
-
-  return (
-    <div className='p-4 ml-4'>
-        <h1 className={"text-2xl font-semibold text-yellow-800 md:text-center "}>LOGIN DEVELOPMENT</h1>
-        <div className="flex justify-around my-4">
-            <div className="flex gap-4">
-                
-                <Dialog>
-                    <DialogTrigger asChild>
-                        <Button variant={"destructive"}>LOGOUT</Button>
-                    </DialogTrigger>
-                    <DialogContent className="max-w-md">
-                        <DialogHeader className="flex items-center">
-                            <DialogTitle className="text-yellow-800 dark:text-yellow-700">
-                                LOGOUT
-                            </DialogTitle>
-                            <DialogDescription>
-                                Are you sure you wish to sign out?
-                            </DialogDescription>
-                        </DialogHeader>
-                        <div className="flex justify-around">
-                            <div className="flex gap-4">
-                                <Button variant={"secondary"}>Yes</Button>
-                                <DialogTrigger asChild>
-                                    <Button variant={"destructive"}>NO</Button>
-                                </DialogTrigger>
-                            </div>
-                        </div>
-                    </DialogContent>
-                </Dialog>
-
-                <Dialog>
-                    <DialogTrigger asChild><Button>LOGIN</Button></DialogTrigger>
-                    <DialogContent className="max-w-lg">
-                        <DialogHeader className="flex items-center">
-                            <DialogTitle className="text-yellow-800 dark:text-yellow-700">LOGIN</DialogTitle>
-                            <DialogDescription>
-                                Please sign in to your account.
-                            </DialogDescription>
-                        </DialogHeader>
-                        <LoginForm />
-                        
-                    </DialogContent>
-                </Dialog>
-
-                
-            </div> 
+    return (
+        <div className='p-4 ml-4'>
+            <h1 className={"text-2xl font-semibold text-yellow-800 md:text-center "}>LOGIN DEVELOPMENT</h1>
+            <div className="flex justify-around my-4">
+                <div className="flex gap-4">
+                    
+                    <LoginForm />
+                    
+                </div> 
+            </div>
         </div>
-
-      
-    </div>
-  );
+    );
 }
--- app/testing/_components/login/loginForm.tsx
+++ app/testing/_components/login/loginForm.tsx
@@ -7,7 +7,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 
 import { fetchUserSession } from '@/lib/auth/fetchUserSession';
-import { useRole, useRoleStore } from "@/components/nav/RoleContext"; 
+import { useRoleStore } from "@/components/nav/RoleContext"; 
 
 import { Button } from "@/components/ui/button"
 import {
@@ -20,6 +20,7 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 
 
 const loginSchema = z.object({
@@ -40,7 +41,7 @@ type UserSessionResponse = {
 
 const LoginForm: React.FC = () => {
     const { toast } = useToast();
-    const { setUserRole } = useRoleStore();
+    const {userRole, setUserRole } = useRoleStore();
     // Use useForm with Zod validation schema
     const formState = useForm<LoginFormValues>({
         resolver: zodResolver(loginSchema),
@@ -66,9 +67,10 @@ const LoginForm: React.FC = () => {
             setUserRole(role)
             toast({
                 title: "Login Status",
-                description: `Welcome ${user?.firstName}`,
+                description: `Welcome ${role.charAt(0).toUpperCase() + role.slice(1)}`,
             })
         } catch (error) {
+            setUserRole("Anonymous")
             console.error("Login error:", error); // Handle error appropriately
             toast({
                 title: "Login Failed",
@@ -78,59 +80,106 @@ const LoginForm: React.FC = () => {
     };
 
     return (
-        <Form {...formState}>
-            <form onSubmit={formState.handleSubmit(onSubmit)} className="space-y-4">
-            {/* Login Field */}
-            <FormField
-                control={formState.control}
-                name="login"
-                render={({ field }) => (
-                <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                    <Input 
-                        placeholder="Enter your username"
-                        autoComplete="username"
-                        {...field} 
-                    />
-                    </FormControl>
-                    <FormMessage />
-                </FormItem>
-                )}
-            />
-            
-            {/* Password Field */}
-            <FormField
-                control={formState.control}
-                name="password"
-                render={({ field }) => (
-                <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                    <Input 
-                        type="password" 
-                        placeholder="Enter your password"
-                        autoComplete="current-password" 
-                        {...field} 
-                    />
-                    </FormControl>
-                   
-                    <FormMessage />
-                </FormItem>
-                )}
-            />
-            
-            {/* Submit Button */}
-            <Button type="submit" disabled={['pending'].includes(mutation.status)}>
-                {mutation.status === 'success' ? 'Success' 
-                : mutation.status === 'pending' ? 'Logging in...' : 'Login'}
-            </Button>
-    
-            {/* Error and Success Messages */}
-            {mutation.isError && <p className="text-red-500">{mutation.error.message}</p>}
-            {mutation.isSuccess && <p className="text-green-500">Login successful!</p>}
-            </form>
-        </Form>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button
+                    variant={userRole === "Anonymous" ? "default" : "destructive"}
+                >
+                    {userRole === "Anonymous" ? "LOGIN" : "LOGOUT"}
+                </Button>
+            </DialogTrigger>
+                        
+            {userRole === "Anonymous" ?
+                <DialogContent className="max-w-lg">
+                    <DialogHeader className="flex items-center">
+                        <DialogTitle className="text-yellow-800 dark:text-yellow-700">LOGIN</DialogTitle>
+                        <DialogDescription>
+                            Please sign in to your account.
+                        </DialogDescription>
+                    </DialogHeader>
+                    
+                    <Form {...formState}>
+                        <form onSubmit={formState.handleSubmit(onSubmit)} className="space-y-4">
+                        {/* Login Field */}
+                        <FormField
+                            control={formState.control}
+                            name="login"
+                            render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Username</FormLabel>
+                                <FormControl>
+                                <Input 
+                                    placeholder="Enter your username"
+                                    autoComplete="username"
+                                    {...field} 
+                                />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                            )}
+                        />
+                        
+                        {/* Password Field */}
+                        <FormField
+                            control={formState.control}
+                            name="password"
+                            render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                <Input 
+                                    type="password" 
+                                    placeholder="Enter your password"
+                                    autoComplete="current-password" 
+                                    {...field} 
+                                />
+                                </FormControl>
+                            
+                                <FormMessage />
+                            </FormItem>
+                            )}
+                        />
+                        
+                        {/* Submit Button */}
+                        <Button type="submit" disabled={['pending'].includes(mutation.status)}>
+                            {mutation.status === 'success' ? 'Success' 
+                            : mutation.status === 'pending' ? 'Logging in...' : 'Login'}
+                        </Button>
+                        
+                        {mutation.isError && <p className="text-red-500">{mutation.error.message}</p>}
+                        {mutation.isSuccess && <p className="text-green-500">Login successful!</p>}
+                        
+                        </form>
+                    </Form>
+                </DialogContent>
+           :
+                <DialogContent className="max-w-md">
+                    <DialogHeader className="flex items-center">
+                        <DialogTitle className="text-yellow-800 dark:text-yellow-700">
+                            LOGOUT
+                        </DialogTitle>
+                        <DialogDescription>
+                            Are you sure you wish to sign out?
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="flex justify-around">
+                        <div className="flex gap-4">
+                            <DialogTrigger asChild>
+                                <Button 
+                                    variant={"secondary"}
+                                    onClick={() => setUserRole("Anonymous")}
+                                >
+                                    Yes
+                                </Button>
+                            </DialogTrigger>
+                            <DialogTrigger asChild>
+                                <Button variant={"destructive"}>NO</Button>
+                            </DialogTrigger>
+                        </div>
+                    </div>
+                </DialogContent>
+           }
+        </Dialog>
     );
 };
 
