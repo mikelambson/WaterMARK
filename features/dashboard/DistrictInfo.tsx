19522ff8942f4dca54f36d7bf8bf7188cf2f206e
mikelambson <macmicool@gmail.com>
2024-12-04 12:52:07 -0800

    Update Code

--- features/dashboard/DistrictInfo.tsx
+++ features/dashboard/DistrictInfo.tsx
@@ -1,11 +1,12 @@
 import { Suspense } from "react";
 import ComponentLoader from '@/features/loader/comploader.module';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { measurementNumber } from '@/lib/utils';
 
 const DistrictInfo = () => {
     return ( 
         <Suspense fallback={<ComponentLoader />}>
-            <Tabs defaultValue="truckee" className="w-full h-full">
+            <Tabs defaultValue="west" className="w-full h-full">
                 <TabsList className="w-full">
                     <TabsTrigger value="truckee">Truckee</TabsTrigger>
                     <TabsTrigger value="west">West</TabsTrigger>
@@ -13,14 +14,14 @@ const DistrictInfo = () => {
                     <TabsTrigger value="east">East</TabsTrigger>
                 </TabsList>
                 <TabsContent value="truckee" className="px-2 h-full">
-                    <div className="px-2 grid grid-rows-3 h-full gap-3">
+                    <div className="px-2 grid grid-rows-3 max-h-full gap-3">
                         <div className="text-2xl font-bold text-center">Truckee</div>
                         <div className="text-lg">Meter data</div>
                         <div className="text-lg">more info</div>
                     </div>
                 </TabsContent>
                 <TabsContent value="west" className="px-2 h-full">
-                    <div className="px-2 grid grid-rows-3 h-full gap-3">
+                    <div className="px-2 grid grid-rows-3 max-h-full gap-3">
                         <div className="text-2xl font-bold text-center">West</div>
                         <div className="text-lg">Meter data</div>
                         <div className="text-lg">more info</div>
