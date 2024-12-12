4d1db4fad8af1eb24d740ec1675deba631aca6c9
mikelambson <macmicool@gmail.com>
2024-12-12 10:44:24 -0800

    Update Code

--- lib/utils/Throttle.tsx
+++ lib/utils/Throttle.tsx
@@ -1,5 +1,15 @@
 import { useRef } from "react";
 
+/**
+ * Throttle hook
+ * @param callback - Function to throttle
+ * @param delay - Delay in milliseconds
+ * @returns Throttled function
+ * @example
+ * const throttledFunction = useThrottle(() => console.log("Throttled function"), 1000);
+ * throttledFunction(); // Will call the function
+ */ 
+
 export const useThrottle = <T extends (...args: any[]) => void>(
   callback: T,
   delay: number
